3271d6141d16cc98216b87de3e048f87f0f92a0c
Amrutaqickitdotnet <velankaramruta@gmail.com>
2022-01-20 01:10:47 +0530

    changed

---
+++ KeywordDrivenProject/src/test/java/keywordEngine/KeywordEngine.java
@@ -0,0 +1,52 @@
+package keywordEngine;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+
+public class KeywordEngine {
+	public Workbook book;
+	public Sheet sh;
+
+	public void readData() {
+
+		String excel_path = "D:\\Amruta\\TestData.xlsx";
+		FileInputStream fs = null;
+		try {
+			fs = new FileInputStream(excel_path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(fs);
+		} catch (EncryptedDocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sh = book.getSheet("OrangeHRM");
+		TestBase.TestBase base = new TestBase.TestBase();
+		for(int i=1 ; i<= sh.getLastRowNum(); i++) {
+		String teststeps =	sh.getRow(i).getCell(0).toString().trim();
+		String locatorType =	sh.getRow(i).getCell(1).toString().trim();
+		String selector =	sh.getRow(i).getCell(2).toString().trim();
+		String action =	sh.getRow(i).getCell(3).toString().trim();
+		String value =	sh.getRow(i).getCell(4).toString().trim();
+		
+		System.out.println(teststeps+":"+ locatorType+":"+selector +":"+ action+":"+value);
+		base.execute(teststeps, locatorType, selector, action, value);
+		}
+		
+
+	}
+
+}
